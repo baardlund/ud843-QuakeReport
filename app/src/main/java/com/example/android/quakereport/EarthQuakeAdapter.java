ffee4172f6cde3f1c7ab6ac4237411a06543b28d
Magnus Bårdlund <baardlund@gmail.com>
2020-05-28 01:46:45 +0200

    2.1 Finished to 25: Visual Polish. 26: Intent next

--- app/src/main/java/com/example/android/quakereport/EarthQuakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthQuakeAdapter.java
@@ -6,7 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.graphics.drawable.GradientDrawable;
 
+import androidx.core.content.ContextCompat;
+
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class EarthQuakeAdapter extends ArrayAdapter<Earthquake> {
@@ -22,31 +26,94 @@ public class EarthQuakeAdapter extends ArrayAdapter<Earthquake> {
         Earthquake ea = getItem(position);
 
         if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_earthquakes, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item, parent, false);
         }
 
-        //Lookup view for datapopulation
-        TextView lm = convertView.findViewById(R.id.list_item_magnitude);
-        TextView m = convertView.findViewById(R.id.magnitude);
-        TextView ll = convertView.findViewById(R.id.list_item_location);
-        TextView l = convertView.findViewById(R.id.location);
-        TextView ld = convertView.findViewById(R.id.list_item_date);
-        TextView d = convertView.findViewById(R.id.date);
-        TextView lu = convertView.findViewById(R.id.list_item_url);
-        TextView u = convertView.findViewById(R.id.url);
+        // Find the TextView with view ID magnitude
+        TextView magnitudeView = (TextView) convertView.findViewById(R.id.magnitude);
+        // Format the magnitude to show 1 decimal place
+        String formattedMagnitude = formatMagnitude(ea.getMagnitude());
+        // Display the magnitude of the current earthquake in that TextView
+        magnitudeView.setText(formattedMagnitude);
 
-        lm.setText(R.string.list_item_magnitude);
+
+        //Lookup view for datapopulation
+        TextView m = magnitudeView;
+        TextView lo = convertView.findViewById(R.id.location_offset);
+        TextView pl = convertView.findViewById(R.id.primary_location);
+        TextView d = convertView.findViewById(R.id.date);
+        TextView t = convertView.findViewById(R.id.time);
+
         m.setText(String.valueOf(ea.getMagnitude()));
 
-        ll.setText(R.string.list_item_location);
-        l.setText(String.valueOf(ea.getLocation()));
+        lo.setText(String.valueOf(ea.getLocationOffset()));
 
-        ld.setText(R.string.list_item_date);
+        pl.setText(String.valueOf(ea.getPrimaryLocation()));
+
         d.setText(String.valueOf(ea.getDate()));
 
-        lu.setText(R.string.list_item_url);
-        u.setText(String.valueOf(ea.getUrl()));
+        t.setText(String.valueOf(ea.getTime()));
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(ea.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+
 
         return convertView;
+    }
+
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+    }
+
+    /**
+     * Return the formatted magnitude string showing 1 decimal place (i.e. "3.2")
+     * from a decimal magnitude value.
+     */
+    private String formatMagnitude(double magnitude) {
+        DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
+        return magnitudeFormat.format(magnitude);
     }
 }
--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,24 +1,36 @@
 package com.example.android.quakereport;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Earthquake {
 
     // @param magnitude earth quake magnitude
     private double magnitude;
 
     // @param city location of earthquake
-    private String location;
+    private String location, distance, direction, city;
 
     // @param date , the  date of the earthquake
-    private long date;
+
+    private long dateUnix;
+    private Date date;
+
 
     private String url;
+
+
 
 
 
     public Earthquake(double magnitude, String location, long date, String url) {
         this.magnitude = magnitude;
         this.location = location;
-        this.date = date;
+        splitLocation(location);
+        this.dateUnix = date;
+        this.date = new Date(date);
         this.url = url;
     }
 
@@ -30,8 +42,59 @@ public class Earthquake {
         return location;
     }
 
-    public long getDate() {
-        return date;
+    private void splitLocation(String location){
+        city="";
+        Pattern pattern = Pattern.compile("^\\d");
+        Matcher m = pattern.matcher(location);
+        if(m.find()) {
+            String[] loc = location.split(" ");
+            distance = loc[0];
+            direction = loc[1];
+
+            for (int i = 3; i <  loc.length; i++) {
+                city += loc[i] + " ";
+            }
+        }
+        else{
+            city = location;
+            distance="";
+            direction="";
+            }
+    }
+
+    public String getLocationOffset(){
+        if(distance=="" && direction=="")
+            return "";
+        else
+            return distance + " " + direction + " of";
+    }
+
+    public String getPrimaryLocation(){
+        return city;
+    }
+
+
+    //dirty code on time formats, clean up later...
+    public String getDateToDisplay(){
+        return formatDate(date) + " " + formatTime(date);
+    }
+
+    public String getDate() {
+        return  formatDate(date);
+    }
+
+    private String formatDate(Date dateObject){
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("DD, MMM, yyyy");
+        return dateFormatter.format(dateObject);
+    }
+
+    public String getTime(){
+        return formatTime(date);
+    }
+
+    private String formatTime(Date dateObject){
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:MM");
+        return timeFormat.format(dateObject);
     }
 
     public String getUrl(){
